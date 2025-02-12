76372f7761cd0125cdc22566926561f5fa4cbcab
Rupesh Pandey <pandeyrupesh00@gmail.com>
2025-02-12 07:33:39 +0000

    Added daily temperatures

--- stack/main.cpp
+++ stack/main.cpp
@@ -4,11 +4,21 @@ using namespace std;
 
 int main(){
     
-    string s;
-    cin >> s;
-    cout << "Input string : " << s << "\n";
-    bool isValid = isValidPrenthesesStack(s);
-    cout << "Is this valid?? : " << isValid;
+    // string s;
+    // cin >> s;
+    // cout << "Input string : " << s << "\n";
+    // bool isValid = isValidPrenthesesStack(s);
+    // cout << "Is this valid?? : " << isValid;
+    int n;
+    int missingNumber;
+    cin >> n;
+    vector<int> arr(n);
+    for (int &x : arr)
+       cin >> x;
+
+    vector<int> output = dailyTemperatures(arr);
+    for(int &x: output)
+        cout << x << " ";
 
 
 }
--- stack/stack_support.h
+++ stack/stack_support.h
@@ -1,62 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class MinStack {
+class MinStack
+{
 public:
-    void push(int val) {
-       if(val < stack.top().second){
-            stack.push({val, val});
-       } else{
-            stack.push({val, stack.top().second});
-       }
-    }
-    
-    void pop() {
-        stack.pop();
-    }
-    
-    int top() {
-        return stack.top().first;
-    }
-    
-    int getMin() {
-        return stack.top().second;
-    }
-private:
-    stack<pair<int, int>> stack; 
-};
-
-bool isValidPrenthesesStack(string s){
-    stack<char> charStack;
-    char temp;
-    for(const char &c : s){
-        switch (c)
+    void push(int val)
+    {
+        if (val < stack.top().second)
         {
-            case '(':
-                charStack.push(')');
-                break;
-            case '[':
-                charStack.push(']');
-                break;
-            case '{':
-                charStack.push('}');
-                break;
-            default:
-                if(charStack.empty())
-                    return false;
-                else{
-                    temp = charStack.top();
-                    if(c == temp)
-                        charStack.pop();
-                    else
-                        return false;
-                }
-                break;
+            stack.push({val, val});
+        }
+        else
+        {
+            stack.push({val, stack.top().second});
         }
     }
-    if(charStack.empty())
+
+    void pop()
+    {
+        stack.pop();
+    }
+
+    int top()
+    {
+        return stack.top().first;
+    }
+
+    int getMin()
+    {
+        return stack.top().second;
+    }
+
+private:
+    stack<pair<int, int>> stack;
+};
+
+bool isValidPrenthesesStack(string s)
+{
+    stack<char> charStack;
+    char temp;
+    for (const char &c : s)
+    {
+        switch (c)
+        {
+        case '(':
+            charStack.push(')');
+            break;
+        case '[':
+            charStack.push(']');
+            break;
+        case '{':
+            charStack.push('}');
+            break;
+        default:
+            if (charStack.empty())
+                return false;
+            else
+            {
+                temp = charStack.top();
+                if (c == temp)
+                    charStack.pop();
+                else
+                    return false;
+            }
+            break;
+        }
+    }
+    if (charStack.empty())
         return true;
     else
         return false;
+}
+
+vector<int> dailyTemperatures(vector<int> &temperatures)
+{
+
+    int size = temperatures.size();
+    stack<pair<int, int>> tempStack;
+    vector<int> output(size);
+    tempStack.push({temperatures[0], 0});
+    for (size_t i = 1; i < size; i++)
+    {   pair<int, int> current = tempStack.top();
+        while (current.first < temperatures[i])
+        {
+            output[current.second] = i - current.second;
+            tempStack.pop();
+            if(tempStack.empty())
+                break;
+            current = tempStack.top();
+        }
+        tempStack.push({temperatures[i], i});
+    }
+    return output;
 }
